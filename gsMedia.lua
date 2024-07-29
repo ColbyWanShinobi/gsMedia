669e54c5eb280e8e9f10be131f043882d93ee3b0
Colby Butler <colby@owljet.com>
2024-07-29 07:52:37 -0400

    first commit

---
+++ gsMedia.lua
@@ -0,0 +1,21 @@
+--[[
+gsMedia
+Created by ColbyWanShinobi
+email: colbywanshinobi@gameshaman.com
+web: gameshaman.com
+repo: https://github.com/ColbyWanShinobi/gsMedia
+--]]
+
+print("gsMedia by gameshaman.com - Addon Loaded");
+
+-- Ensure LibSharedMedia is available
+local LSM = LibStub:GetLibrary("LibSharedMedia-3.0")
+
+-- Register your texture
+LSM:Register("statusbar", "gsBarTexture", [[Interface\AddOns\gsMedia\media\textures\gsBarTexture.tga]])
+
+-- Register your font
+LSM:Register("font", "Liberation Sans Regular", [[Interface\AddOns\gsMedia\media\fonts\LiberationSans-Regular.ttf]])
+
+--Print a status message
+print("gsMedia - Loaded");
